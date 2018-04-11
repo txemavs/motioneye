d72ee9c7f85d7a1d1320ae6e3ad565a76714878b
txema <txema@nabla.net>
2018-04-11 15:19:07 +0200

    sphinx docs
    Created a conf.py and included RtD theme

---
+++ docs/conf.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import time
+import datetime
+import subprocess
+
+rootpath = os.path.abspath('../')
+sys.path.insert(0, rootpath)
+
+from mock import Mock
+sys.modules['fcntl'] = Mock()
+sys.modules['pycurl'] = Mock()
+import fcntl
+import pycurl
+import motioneye
+
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.todo',
+]
+
+project = u'Motioneye'
+copyright = u'2013 Calin Crisan'
+version = motioneye.VERSION
+
+add_module_names = False
+autodoc_member_order='groupwise'
+autodoc_mock_imports = ['fcntl', 'pycurl']
+autosummary_generate = True
+
+exclude_patterns = ['_build' ]
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': True,
+    'display_version': True
+}
+
+html_title = "Motioneye %s" % (version)
+html_logo = '../extra/motioneye-64x64.png'
+html_short_title = "Motioneye v%s documentation " % (version)
+html_domain_indices = True
+html_use_index = True
+html_split_index = True
+html_show_sourcelink = False
+htmlhelp_basename = 'meyedoc'
+
+master_doc = 'index'
+modindex_common_prefix = ['motioneye.']
+
+
+def run_apidoc():
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    package_path = os.path.abspath(os.path.dirname(cur_dir))
+    output_path = os.path.join(cur_dir, 'api')
+    cmd_path = 'sphinx-apidoc'
+    if hasattr(sys, 'real_prefix'):
+        d = os.listdir(sys.prefix)
+    else:
+        d = os.listdir(os.path.dirname(sys.executable))
+    for folder in ['bin', 'Scripts']:
+        if folder in d:
+            cmd_path = os.path.abspath(os.path.join(sys.prefix, folder, 'sphinx-apidoc'))
+    print("sphinx apidoc read %s" % (package_path) )
+    print("sphinx apidoc write %s" % (output_path) )
+    print("Execute %s" % cmd_path)
+    subprocess.check_call([cmd_path, '--force', '--separate', '-o', output_path, package_path])
+
+
+run_apidoc()   
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+Motioneye documentation
+=======================
+
+motionEye is a web frontend for the motion daemon, written in Python.
+
+.. toctree::
+   :caption: Documentation
+
+   genindex-all
+   api/motioneye
+
+
+
+Related Documentation
+---------------------
+
+* `MotionEye Wiki <https://github.com/ccrisan/motioneye/wiki>`_
+* `MotionEyeOS Wiki <https://github.com/ccrisan/motioneyeos/wiki>`_
+* `Motion Documentation <https://motion-project.github.io/motion_guide.html>`_
+
+
